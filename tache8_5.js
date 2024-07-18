1f0a843a32cbae161b96ae78fc6d26f6ff4c6b7a
Mouhamed Ndiaye <mouhamethndiaye123@gmail.com>
2024-07-18 09:50:06 +0000

    ajout des fichiers du tache8 js exo5

---
+++ tache8_5.js
@@ -0,0 +1,7 @@
+// Utilisez la méthode .forEach() pour imprimer chaque élément d'un tableau.
+
+let tab = ['Moussa', 'Ousseynou', 9, 'Abdou', 'Mahmoud', 'Nafina', 'Tyma']
+
+tab.forEach((element)=>
+    console.log(element)
+)
